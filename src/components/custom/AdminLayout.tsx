b95cc5d9840d0b1a2df7da238770ac704e24005f
HossamBhi <hossambasha9900@gmail.com>
2024-11-30 08:41:03 +0200

    Fix rtl

---
+++ src/components/custom/AdminLayout.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Outlet } from "react-router";
+import Sidebar from "./sidebar";
+
+const AdminLayout = () => {
+  // const [isCollapsed, setIsCollapsed] = useIsCollapsed();
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  return (
+    <div className="relative h-full overflow-hidden bg-background">
+      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+      <main
+        id="content"
+        className={`overflow-x-hidden pt-16 transition-[margin] md:overflow-y-hidden md:pt-0 ${
+          isCollapsed ? "md:mr-14" : "md:mr-64"
+        } h-full`}
+      >
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AdminLayout;
