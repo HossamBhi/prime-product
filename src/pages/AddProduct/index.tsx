28968ef79a645fdf5f9884cc2b18f4073b7ed3fa
HossamBhi <hossambasha9900@gmail.com>
2024-11-30 17:34:21 +0200

    Add page title

--- src/pages/AddProduct/index.tsx
+++ src/pages/AddProduct/index.tsx
@@ -9,12 +9,21 @@ const AddProduct = () => {
   return (
     <Layout>
       {/* ===== Top Heading ===== */}
-      <Layout.Header>
+      <Layout.Header className="flex flex-col pb-0">
         <PageHeader />
+        <div className="flex items-center justify-between w-full pt-5">
+          <h1 className="text-[#49296A] text-[20px]">اضافة منتج جديد</h1>
+          <div className="text-sm relative flex flex-row w-[140px] h-[30px]">
+            <p className="bg-[#A3D936] px-2 py-1 text-white rounded-full top-0 bottom-0 text-center absolute w-[110px] right-0">
+              مفعل
+            </p>
+            <p className="bg-[#ACACAC] w-full px-2 py-1 text-white rounded-full"></p>
+          </div>
+        </div>
       </Layout.Header>
 
       {/* ===== Main ===== */}
-      <Layout.Body>
+      <Layout.Body className="pt-[12px]">
         <div className="space-y-4">
           <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4"></div>
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
